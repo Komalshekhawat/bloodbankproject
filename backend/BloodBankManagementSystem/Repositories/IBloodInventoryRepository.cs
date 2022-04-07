fa970a2c0756293878f38f0e4f101d1f8f3796a2
abcd <ab@try.com>
2022-04-07 13:41:24 +0530

    Inventory

---
+++ backend/BloodBankManagementSystem/Repositories/IBloodInventoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BloodBankManagementSystem.Entities;
+namespace BloodBankManagementSystem.Repositories
+{
+    public interface IBloodInventoryRepository
+    {
+        List<BloodInventory> GetAllBloodInventories();
+        BloodInventory GetBloodInventories(int BloodInventoryID);
+        void Update(BloodInventory bloodInventory);
+        void Delete(int BloodInventoryID);
+
+
+    }
+}
