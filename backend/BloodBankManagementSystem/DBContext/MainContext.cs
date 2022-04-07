f629e85a1151e72c20988d88383da3833c6c8b1a
kummithavikas@gmail.com <kummithavikas@gmail.com>
2022-04-07 01:27:58 -0700

    Hospital

--- backend/BloodBankManagementSystem/DBContext/MainContext.cs
+++ backend/BloodBankManagementSystem/DBContext/MainContext.cs
@@ -16,6 +16,7 @@ namespace BloodBankManagementSystem.DBContext
             public DbSet<BloodDonationCamp> BloodDonationCamps{get;set;}
         public DbSet<BloodDonor> BloodDonors { get; set; }
         public DbSet<BloodDonorDonation> BloodDonorDonations { get; set; }
+        public DbSet<Hospital> Hospitals { get; set; }
 
 
 
